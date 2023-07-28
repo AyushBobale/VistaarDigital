d63212414a21a423c871cb26303bd87cb684835d
Ayush Bobale <bobaleayush@gmail.com>
2023-07-29 00:13:03 +0530

    final

--- source/client/src/pages/Home/Home.jsx
+++ source/client/src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
+import "./Home.css";
+
+import { Link } from "react-router-dom";
 import React from "react";
 import { useServerCheckQuery } from "../../redux/slices/rootApi";
 
 export const Home = () => {
   const serverCheck = useServerCheckQuery();
   console.log(serverCheck);
-  return <div>Home</div>;
+  return (
+    <div className="home-wrap">
+      <h2>Home</h2>
+      <Link to="https://github.com/AyushBobale/VistaarDigital">
+        Project Link
+      </Link>
+    </div>
+  );
 };
